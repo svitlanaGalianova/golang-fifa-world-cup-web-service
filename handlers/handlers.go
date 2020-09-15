8c4f6a6f139593f80e98e27e41d32aa67f852543
Svitlana Galianova <Svitlana_Galianova@manulife.com>
2020-09-15 14:46:54 -0400

    created POST handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
+	//return 204
 	res.WriteHeader(http.StatusNoContent)
 }
 
@@ -17,6 +18,7 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 	if year == "" {
 		winners, err := data.ListAllJSON()
 		if err != nil {
+			//return 500
 			res.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -24,6 +26,7 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 	} else {
 		filteredWinners, err := data.ListAllByYear(year)
 		if err != nil {
+			//return 400
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
@@ -34,7 +37,23 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isTokenValid := data.IsAccessTokenValid(accessToken)
 
+	if !isTokenValid {
+		//return 401
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		err := data.AddNewWinner(req.Body)
+
+		if err != nil {
+			//return 422
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		//return 201
+		res.WriteHeader(http.StatusCreated)
+	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
